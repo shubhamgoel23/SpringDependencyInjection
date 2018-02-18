816314f66d2f167d7df90f9895c2a64593bddeac
Shubham Goel <shubhamgoyal23@yahoo.com>
2018-02-18 14:42:42 +0530

    spring dependency injection

---
+++ src/main/java/com/spring/didemo/controller/MyController.java
@@ -0,0 +1,12 @@
+package com.spring.didemo.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyController {
+
+    public void hello(){
+        System.out.println("hello!!!");
+    }
+
+}
