45e65aa3ed1e721f4ea0c1c93074b8f27e55785f
Shubham Goel <shubhamgoyal23@yahoo.com>
2018-04-21 11:18:55 +0530

    accessing environment properties

--- src/main/java/com/spring/didemo/config/PropertyConfig.java
+++ src/main/java/com/spring/didemo/config/PropertyConfig.java
@@ -1,15 +1,20 @@
 package com.spring.didemo.config;
 
 import com.spring.didemo.model.FakeDatasource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
 
 @Configuration
 @PropertySource("classpath:datasource.properties")
 public class PropertyConfig {
+
+    @Autowired
+    Environment env;
 
     @Value("${org.username}") // spring expression lang
     String user;
@@ -23,7 +28,8 @@ public class PropertyConfig {
     @Bean
     public FakeDatasource fakeDatasource (){
         FakeDatasource fakeDatasource = new FakeDatasource();
-        fakeDatasource.setUser(user);
+//        fakeDatasource.setUser(user);
+        fakeDatasource.setUser(env.getProperty("USERNAME"));
         fakeDatasource.setPassword(password);
         fakeDatasource.setUrl(url);
         return  fakeDatasource;
